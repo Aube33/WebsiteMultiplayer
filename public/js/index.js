77d70d07ada7e02cce608243e69296c93f2574fa
Aube <43349927+Aube33@users.noreply.github.com>
2022-07-03 12:36:32 +0200

    Update index.js
    Added the possibility to send mouse cursor type information on hover text areas and buttons

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,5 @@
 var mousex, mousey;
-const PointorsSize=(screen.width+screen.height)/17000
+const PointorsSize=(screen.width+screen.height)/18000
 
 let players = [];
 let LocalPlayer;
@@ -15,6 +15,7 @@ const ctx = canvas.getContext('2d');
 //=== PLAYERS ===
 //Draw Mouse in canva
 function drawMouse(xoff, yoff, xmul, ymul, color, type) {
+    //Default cursor
     if(type==0){
         ctx.beginPath()
         ctx.lineTo(2 * xmul + xoff, 1 * ymul + yoff);
@@ -29,8 +30,55 @@ function drawMouse(xoff, yoff, xmul, ymul, color, type) {
         ctx.closePath();
         ctx.stroke();
     }
+    //Text cursor
     else if(type==1){
-        
+        ctx.beginPath()
+        ctx.lineTo(2 * xmul + xoff, 104 * ymul + yoff);
+        ctx.lineTo(52 * xmul + xoff, 104 * ymul + yoff);
+        ctx.lineTo(53 * xmul + xoff, 99 * ymul + yoff);
+        ctx.lineTo(32 * xmul + xoff, 99 * ymul + yoff);
+        ctx.lineTo(32 * xmul + xoff, 7 * ymul + yoff);
+        ctx.lineTo(53 * xmul + xoff, 5 * ymul + yoff);
+        ctx.lineTo(53 * xmul + xoff, 1 * ymul + yoff);
+        ctx.lineTo(1 * xmul + xoff, 0 * ymul + yoff);
+        ctx.lineTo(2 * xmul + xoff, 6 * ymul + yoff);
+        ctx.lineTo(24 * xmul + xoff, 6 * ymul + yoff);
+        ctx.lineTo(25 * xmul + xoff, 99 * ymul + yoff);
+        ctx.lineTo(2 * xmul + xoff, 99 * ymul + yoff);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
+    }
+    //Pointer cursor
+    else if(type==2){
+        ctx.beginPath()
+        ctx.lineTo(39 * xmul + xoff, 117 * ymul + yoff);
+        ctx.lineTo(83 * xmul + xoff, 117 * ymul + yoff);
+        ctx.bezierCurveTo(96 * xmul + xoff, 103 * ymul + yoff, 106 * xmul + xoff, 40 * ymul + yoff, 93 * xmul + xoff, 37 * ymul + yoff)
+        ctx.lineTo(82 * xmul + xoff, 37 * ymul + yoff);
+        ctx.lineTo(82 * xmul + xoff, 59 * ymul + yoff);
+        ctx.lineTo(77 * xmul + xoff, 58 * ymul + yoff);
+        ctx.lineTo(77 * xmul + xoff, 32 * ymul + yoff);
+        ctx.lineTo(65 * xmul + xoff, 31 * ymul + yoff);
+        ctx.lineTo(65 * xmul + xoff, 53 * ymul + yoff);
+        ctx.lineTo(60 * xmul + xoff, 52 * ymul + yoff);
+        ctx.lineTo(60 * xmul + xoff, 26 * ymul + yoff);
+        ctx.lineTo(48 * xmul + xoff, 26 * ymul + yoff);
+        ctx.lineTo(47 * xmul + xoff, 51 * ymul + yoff);
+        ctx.lineTo(42 * xmul + xoff, 51 * ymul + yoff);
+        ctx.lineTo(42 * xmul + xoff, 3 * ymul + yoff);
+        ctx.lineTo(37 * xmul + xoff, 1 * ymul + yoff);
+        ctx.lineTo(31 * xmul + xoff, 2 * ymul + yoff);
+        ctx.lineTo(29 * xmul + xoff, 62 * ymul + yoff);
+        ctx.lineTo(25 * xmul + xoff, 62 * ymul + yoff);
+        ctx.lineTo(15 * xmul + xoff, 53 * ymul + yoff);
+        ctx.lineTo(9 * xmul + xoff, 53 * ymul + yoff);
+        ctx.lineTo(6 * xmul + xoff, 59 * ymul + yoff);
+        ctx.fillStyle = color;
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
     }
 }
 
@@ -41,6 +89,27 @@ document.addEventListener("mousemove", () => {
     screen = [window.innerWidth, window.innerHeight];
     socket.emit('mouse_position', {mx: mousex, my: mousey, myScreen: screen});
 }); 
+//Send MY mouse cursor state
+let textareas = document.getElementsByTagName("textarea");
+for(var i=0; i<textareas.length; i++){
+    textareas[i].addEventListener('mouseenter',function(e){
+        socket.emit('mouse_cursor', {cursor: 1});
+    });
+    
+    textareas[i].addEventListener('mouseout',function(e){
+        socket.emit('mouse_cursor', {cursor: 0});
+    });
+}
+let buttons = document.getElementsByTagName("button");
+for(var i=0; i<buttons.length; i++){
+    buttons[i].addEventListener('mouseenter',function(e){
+        socket.emit('mouse_cursor', {cursor: 2});
+    });
+    
+    buttons[i].addEventListener('mouseout',function(e){
+        socket.emit('mouse_cursor', {cursor: 0});
+    });
+}
 
 //Add players name
 function drawPlayers_name(x, y, name){
@@ -54,13 +123,46 @@ function drawPlayers() {
     count=0
     players.forEach(function({x, y, name, color, id, screen, type}) {
         if(id!=socket.id){
-            x_sync=x*(window.innerWidth/screen[0]);
-            y_sync=y*(window.innerHeight/screen[1]);
-            drawMouse(x_sync, y_sync, PointorsSize, PointorsSize, color, type)
-            drawPlayers_name(x_sync+13,y_sync+7,name)
+            if(type==0){
+                x_sync=x*(window.innerWidth/screen[0]);
+                y_sync=y*(window.innerHeight/screen[1]);
+                drawMouse(x_sync, y_sync, PointorsSize, PointorsSize, color, type)
+                drawPlayers_name(x_sync+14,y_sync+7,name)
+            }
+            if(type==1){
+                x_sync=x*(window.innerWidth/screen[0]);
+                y_sync=y*(window.innerHeight/screen[1]);
+                drawMouse(x_sync-1, y_sync-7, PointorsSize, PointorsSize, color, type)
+                drawPlayers_name(x_sync+14,y_sync+7,name)     
+            }
+            if(type==2){
+                x_sync=x*(window.innerWidth/screen[0]);
+                y_sync=y*(window.innerHeight/screen[1]);
+                drawMouse(x_sync-5, y_sync+3, PointorsSize, PointorsSize, color, type)
+                drawPlayers_name(x_sync+14,y_sync+7,name)     
+            }
         }
         else{
             LocalPlayer=players[count]
+            /*
+            if(type==0){
+                x_sync=x*(window.innerWidth/screen[0]);
+                y_sync=y*(window.innerHeight/screen[1]);
+                drawMouse(x_sync, y_sync, PointorsSize, PointorsSize, color, type)
+                drawPlayers_name(x_sync+14,y_sync+7,name)
+            }
+            if(type==1){
+                x_sync=x*(window.innerWidth/screen[0]);
+                y_sync=y*(window.innerHeight/screen[1]);
+                drawMouse(x_sync-1, y_sync-7, PointorsSize, PointorsSize, color, type)
+                drawPlayers_name(x_sync+14,y_sync+7,name)     
+            }
+            if(type==2){
+                x_sync=x*(window.innerWidth/screen[0]);
+                y_sync=y*(window.innerHeight/screen[1]);
+                drawMouse(x_sync-5, y_sync+3, PointorsSize, PointorsSize, color, type)
+                drawPlayers_name(x_sync+14,y_sync+7,name)     
+            }*/
         }
         count+=1;
       });
